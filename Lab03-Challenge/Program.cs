f3c05e1a9772f2220720b70c211249ea7ddb7591
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-06-18 20:08:15 +0300

    add text

--- Lab03-Challenge/Program.cs
+++ Lab03-Challenge/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab03_Challenge
+﻿using System.Diagnostics.Metrics;
+
+namespace Lab03_Challenge
 {
     internal class Program
     {
@@ -6,36 +8,75 @@
 
         static void Main(string[] args)
         {
+
+            
             Console.WriteLine("Please enter 3 number");
           string Number  =Console.ReadLine();
             string[] Numbers = Number.Split(" ");
             Console.WriteLine($"the multiplication {Multiplication(Numbers)}");
+            /*
+            Console.WriteLine("Please enter number between 2-10");
+            string Number = Console.ReadLine();
+            if(Number >2 && Number > 10)
+            {
+
+            }
+*/
+
         }
         public static int Multiplication(string[] Array)
         {
             
             int counter=0;
+
+            
             foreach (string num in Array) {
-                if(Array.Length < 3)
-                {
-                    return 0;
-
-                }
-                else
-                {
-                sum *= Convert.ToInt32(num);
-                counter++;
-              
-
                 if (counter == 3)
                 {
                         
                         return sum;
                 }
+
+                if(Array.Length < 3)
+                {
+                    return 0;
+
+                }
+
+
+                else
+                {
+                    try
+                    {
+                        if (Convert.ToInt32(num) > 0) {
+                     sum *= Convert.ToInt32(num);
+                counter++;
+              
+
+               
+                    }
+
+                    }
+                    catch (Exception)
+                    {
+                        Array[counter] = "1";
+                        sum *= Convert.ToInt32(Array[counter]);
+                    }
+
+
                 }
                
             }
             return sum;
+        }
+        public static void Randoms(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Random random = new Random();
+                Console.WriteLine($"{i} of num" );
+            }
+
         }
     }
    
