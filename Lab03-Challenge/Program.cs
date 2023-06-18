340262823c8d9475856c940910ea85d791379dd1
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-06-18 19:13:46 +0300

    Add project files.

---
+++ Lab03-Challenge/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab03_Challenge
+{
+    internal class Program
+    {
+        public static int sum = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 3 number");
+          string Number  =Console.ReadLine();
+            string[] Numbers = Number.Split(" ");
+            Console.WriteLine($"the multiplication {Multiplication(Numbers)}");
+        }
+        public static int Multiplication(string[] Array)
+        {
+            
+            int counter=0;
+            foreach (string num in Array) {
+                if(Array.Length < 3)
+                {
+                    return 0;
+
+                }
+                else
+                {
+                sum *= Convert.ToInt32(num);
+                counter++;
+              
+
+                if (counter == 3)
+                {
+                        
+                        return sum;
+                }
+                }
+               
+            }
+            return sum;
+        }
+    }
+   
+
+}
